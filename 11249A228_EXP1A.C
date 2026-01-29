f885829b87763eddfa51880b80a5bb437c0a4665
Monish S.M <11249a228@kanchiuniv.ac.in>
2026-01-29 11:20:55 +0530

    Update 11249A228_EXP1A.C

--- 11249A228_EXP1A.C
+++ 11249A228_EXP1A.C
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-// Recursive function to compute the nth Fibonacci number
 int fibonacci(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);  // Recursive calls for F(n-1) + F(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2); 
 }
 
 int main() {
@@ -15,9 +14,9 @@ int main() {
     
     printf("Fibonacci series: ");
     for (i = 0; i < terms; i++) {
-        printf("%d ", fibonacci(i));  // Print each term using recursion
+        printf("%d ", fibonacci(i)); 
     }
     printf("\n");
-    
+
     return 0;
 }
